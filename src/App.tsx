370b37dba50c66f3051093e362341892429f53f9
Raphael Lima <phaelslima@gmail.com>
2022-09-08 21:02:26 -0300

    feat: configure navigation pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { BrowserRouter } from 'react-router-dom'
+
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Hello, World!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
