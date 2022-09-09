9bab6326ba701d5c06ef8128a8b0df38d448b4ee
Raphael Lima <phaelslima@gmail.com>
2022-09-09 11:12:41 -0300

    feat: add header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 4rem;
+
+  height: 18.5rem;
+
+  background-image: url('images/header-bg.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+`
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '../../components/Header'
+
 export function DefaultLayout() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   )
